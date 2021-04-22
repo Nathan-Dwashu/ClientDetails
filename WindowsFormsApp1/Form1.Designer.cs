008aad7f55d75252acd7f596392a6564a5d2669e
Nathan-Dwashu <nathandwashu99@gmail.com>
2021-04-22 17:36:23 +0200

    Testing Purposes

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,323 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.TreeNode treeNode67 = new System.Windows.Forms.TreeNode("Cape Winelands District Municipality");
+            System.Windows.Forms.TreeNode treeNode68 = new System.Windows.Forms.TreeNode("Central Karoo District Municipality");
+            System.Windows.Forms.TreeNode treeNode69 = new System.Windows.Forms.TreeNode("City of Cape Town Metropolitan Municipality");
+            System.Windows.Forms.TreeNode treeNode70 = new System.Windows.Forms.TreeNode("Garden Route District Municipality");
+            System.Windows.Forms.TreeNode treeNode71 = new System.Windows.Forms.TreeNode("Overberg District Municipality");
+            System.Windows.Forms.TreeNode treeNode72 = new System.Windows.Forms.TreeNode("West Coast District Municipality");
+            System.Windows.Forms.TreeNode treeNode73 = new System.Windows.Forms.TreeNode("Western Cape", new System.Windows.Forms.TreeNode[] {
+            treeNode67,
+            treeNode68,
+            treeNode69,
+            treeNode70,
+            treeNode71,
+            treeNode72});
+            System.Windows.Forms.TreeNode treeNode74 = new System.Windows.Forms.TreeNode("Alfred Nzo District Municipality");
+            System.Windows.Forms.TreeNode treeNode75 = new System.Windows.Forms.TreeNode("\tAmathole District Municipality");
+            System.Windows.Forms.TreeNode treeNode76 = new System.Windows.Forms.TreeNode("\tBuffalo City Metropolitan Municipality");
+            System.Windows.Forms.TreeNode treeNode77 = new System.Windows.Forms.TreeNode("Chris Hani District Municipality");
+            System.Windows.Forms.TreeNode treeNode78 = new System.Windows.Forms.TreeNode("Joe Gqabi District Municipality");
+            System.Windows.Forms.TreeNode treeNode79 = new System.Windows.Forms.TreeNode("Nelson Mandela Bay Metropolitan Municipality");
+            System.Windows.Forms.TreeNode treeNode80 = new System.Windows.Forms.TreeNode("OR Tambo District Municipality");
+            System.Windows.Forms.TreeNode treeNode81 = new System.Windows.Forms.TreeNode("Sarah Baartman District Municipality");
+            System.Windows.Forms.TreeNode treeNode82 = new System.Windows.Forms.TreeNode("Eastern Cape", new System.Windows.Forms.TreeNode[] {
+            treeNode74,
+            treeNode75,
+            treeNode76,
+            treeNode77,
+            treeNode78,
+            treeNode79,
+            treeNode80,
+            treeNode81});
+            System.Windows.Forms.TreeNode treeNode83 = new System.Windows.Forms.TreeNode("City of Ekurhuleni Metropolitan Municipality");
+            System.Windows.Forms.TreeNode treeNode84 = new System.Windows.Forms.TreeNode("City of Johannesburg Metropolitan Municipality");
+            System.Windows.Forms.TreeNode treeNode85 = new System.Windows.Forms.TreeNode("City of Tshwane Metropolitan Municipality");
+            System.Windows.Forms.TreeNode treeNode86 = new System.Windows.Forms.TreeNode("Sedibeng District Municipality");
+            System.Windows.Forms.TreeNode treeNode87 = new System.Windows.Forms.TreeNode("West Rand District Municipality");
+            System.Windows.Forms.TreeNode treeNode88 = new System.Windows.Forms.TreeNode("Gauteng", new System.Windows.Forms.TreeNode[] {
+            treeNode83,
+            treeNode84,
+            treeNode85,
+            treeNode86,
+            treeNode87});
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Address";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(73, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(73, 54);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(346, 52);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(48, 17);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Male";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(346, 88);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(59, 17);
+            this.radioButton2.TabIndex = 7;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Female";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(346, 150);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(40, 17);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.Text = "C#";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(346, 179);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(47, 17);
+            this.checkBox2.TabIndex = 9;
+            this.checkBox2.Text = "ASP";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(346, 211);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(49, 17);
+            this.checkBox3.TabIndex = 10;
+            this.checkBox3.Text = "Java";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(346, 276);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(483, 20);
+            this.treeView1.Name = "treeView1";
+            treeNode67.Name = "Node1";
+            treeNode67.Text = "Cape Winelands District Municipality";
+            treeNode68.Name = "Node6";
+            treeNode68.Text = "Central Karoo District Municipality";
+            treeNode69.Name = "Node7";
+            treeNode69.Text = "City of Cape Town Metropolitan Municipality";
+            treeNode70.Name = "Node8";
+            treeNode70.Text = "Garden Route District Municipality";
+            treeNode71.Name = "Node9";
+            treeNode71.Text = "Overberg District Municipality";
+            treeNode72.Name = "Node10";
+            treeNode72.Text = "West Coast District Municipality";
+            treeNode73.Name = "Node0";
+            treeNode73.Text = "Western Cape";
+            treeNode74.Name = "Node12";
+            treeNode74.Text = "Alfred Nzo District Municipality";
+            treeNode75.Name = "Node13";
+            treeNode75.Text = "\tAmathole District Municipality";
+            treeNode76.Name = "Node14";
+            treeNode76.Text = "\tBuffalo City Metropolitan Municipality";
+            treeNode77.Name = "Node15";
+            treeNode77.Text = "Chris Hani District Municipality";
+            treeNode78.Name = "Node16";
+            treeNode78.Text = "Joe Gqabi District Municipality";
+            treeNode79.Name = "Node17";
+            treeNode79.Text = "Nelson Mandela Bay Metropolitan Municipality";
+            treeNode80.Name = "Node18";
+            treeNode80.Text = "OR Tambo District Municipality";
+            treeNode81.Name = "Node19";
+            treeNode81.Text = "Sarah Baartman District Municipality";
+            treeNode82.Name = "Node11";
+            treeNode82.Text = "Eastern Cape";
+            treeNode83.Name = "Node25";
+            treeNode83.Text = "City of Ekurhuleni Metropolitan Municipality";
+            treeNode84.Name = "Node26";
+            treeNode84.Text = "City of Johannesburg Metropolitan Municipality";
+            treeNode85.Name = "Node27";
+            treeNode85.Text = "City of Tshwane Metropolitan Municipality";
+            treeNode86.Name = "Node28";
+            treeNode86.Text = "Sedibeng District Municipality";
+            treeNode87.Name = "Node29";
+            treeNode87.Text = "West Rand District Municipality";
+            treeNode88.Name = "Node24";
+            treeNode88.Text = "Gauteng";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode73,
+            treeNode82,
+            treeNode88});
+            this.treeView1.Size = new System.Drawing.Size(291, 129);
+            this.treeView1.TabIndex = 12;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApp1.Properties.Resources.download;
+            this.pictureBox1.Location = new System.Drawing.Point(483, 167);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(291, 173);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Location = new System.Drawing.Point(33, 20);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(211, 93);
+            this.groupBox1.TabIndex = 14;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "User Details";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "Bread",
+            "Milk",
+            "Eggs",
+            "Spice"});
+            this.listBox1.Location = new System.Drawing.Point(33, 150);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(211, 134);
+            this.listBox1.TabIndex = 15;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(833, 361);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.Name = "Form1";
+            this.Text = "Client Details";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string text = listBox1.GetItemText(listBox1.SelectedItem);
+            MessageBox.Show(text);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String name = textBox1.Text;
+            String address = textBox2.Text;
+
+            MessageBox.Show($"Hello {name}. Your address is {address}");
+        }
+    }
+}
